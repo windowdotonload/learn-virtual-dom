ecd59d5d5602cf9de3d9bddba3c6270d67e73e0e
windowdotonload <275981471@qq.com>
2021-07-12 22:27:35 +0800

    mod

--- src/index.js
+++ src/index.js
@@ -16,9 +16,21 @@ import {
 const patch = init([classModule, propsModule, styleModule, eventListenersModule])
 
 // 创建虚拟节点
-const myVnode = h('a', { props: { href: '' } }, h('div', { class: { class1: true } }, 'this is div'))
+const myVnode = h('div', { props: { href: '' } }, [
+    h('div', 'this is div-sub1'),
+    h('div', 'this is div-sub2')
+])
 console.log(myVnode)   //h函数返回一个vnode对象,用于patch渲染
+const myVnode1 = h('div', { props: { href: '' } }, [
+    h('div', 'this is div-sub1'),
+    h('div', 'this is div-sub3'),
+    h('div', 'this is div-sub2')
+])
 
 // 让虚拟节点上树
+const btn = document.getElementById('btn')
 const continer = document.getElementById('container')
+btn.onclick = () => {
+    patch(myVnode, myVnode1)
+}
 patch(continer, myVnode)
