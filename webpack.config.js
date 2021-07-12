a5cbcaca7a0ac231d79e210b5806dd7ab74a9dad
windowdotonload <275981471@qq.com>
2021-07-12 16:03:03 +0800

    mod

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: windowdotonload
+ */
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: windowdotonload
+ */
+const path = require('path');
+
+module.exports = {
+    // 如果没有htmlwebpackplugin，那么webpack命令生成的文件只包含bundle.js
+    entry: './src/index.js',
+    output: {
+        // publicPath
+        publicPath: "",
+        filename: 'bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        // 静态资源文件夹
+        contentBase: "www"
+    }
+};
