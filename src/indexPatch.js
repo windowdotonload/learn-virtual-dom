35e01dc2e42eeab2f6626f96e6208e14eedf56c2
windowdotonload <275981471@qq.com>
2021-07-15 16:42:02 +0800

    mod

--- src/indexPatch.js
+++ src/indexPatch.js
@@ -12,12 +12,12 @@ import h from './mysnabbdom/h.js'
 import patch from './mysnabbdom/patch.js'
 
 const myVnode1 = h('h1', {}, [
-    h('div', {}, 'this is sub-div1'),
-    h('div', {}, 'this is sub-div1'),
+    h('div', { key: 'A' }, 'this is sub-div1'),
+    h('div', { key: 'B' }, 'this is sub-div1'),
 ])
 const myVnode2 = h('h1', {}, [
-    h('div', {}, 'this is sub-div1'),
-    h('div', {}, 'this is sub-div2'),
+    h('div', { key: 'A' }, 'this is sub-div1'),
+    h('div', { key: 'B' }, 'this is sub-div2'),
 ])
 const container = document.getElementById('container')
 patch(container, myVnode1)
--- src/mysnabbdom/updateChildre.js
+++ src/mysnabbdom/updateChildre.js
@@ -3,6 +3,11 @@
  * @version:
  * @Author: windowdotonload
  */
+import patchVnode from "./patchVnode"
+function checkSameVndoe(a, b) {
+    return a.sel == b.sel && a.key == b.key
+}
+
 export default function updateChildren(parentElm, oldCh, newCh) {
     let oldStartIdx = 0
     let newStartIdx = 0
@@ -12,4 +17,18 @@ export default function updateChildren(parentElm, oldCh, newCh) {
     let newStartVnode = newCh[0]
     let oldEndVnode = oldCh[oldEndIdx]
     let newEndVnode = newCh[newEndIdx]
+
+    while (oldStartIdx <= oldEndIdx && newStartIdx <= newEndIdx) {
+        // 新前与旧前
+        if (checkSameVndoe(oldStartVnode, newStartVnode)) {
+            patchVnode(oldStartVnode, newStartVnode)
+            oldStartVnode = oldCh[++oldStartIdx]
+            newStartVnode = newCh[++newStartIdx]
+        } else if (checkSameVndoe(oldEndVnode, newEndVnode)) {
+            // 新后与旧后
+            patchVnode(oldStartVnode, newStartVnode)
+            oldStartVnode = oldCh[--oldStartIdx]
+            newStartVnode = newCh[--newStartIdx]
+        }
+    }
 }
